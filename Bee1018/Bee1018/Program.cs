551a02909f6319fea1e5f76e3f042869f536d63c
vsoares99 <victor.o.soares@hotmail.com>
2024-08-26 17:21:28 -0300

    Resolvido BEE1018

--- Bee1018/Bee1018/Program.cs
+++ Bee1018/Bee1018/Program.cs
@@ -6,9 +6,37 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            int valor = int.Parse(Console.ReadLine());
+            int N;
+            N = int.Parse(Console.ReadLine());
+            
+            int notas100 = N / 100;
+            N %= 100;
 
+            int notas50 = N / 50;
+            N %= 50;
 
+            int notas20 = N / 20;
+            N %= 20;
+
+            int notas10 = N / 10;
+            N %= 10;
+
+            int notas5 = N / 5;
+            N %= 5;
+
+            int notas2 = N / 2;
+            N %= 2;
+
+            int notas1 = N / 1;
+            N %= 1;
+
+            Console.WriteLine($"{notas100} nota(s) de R$ 100,00");
+            Console.WriteLine($"{notas50} nota(s) de R$ 50,00");
+            Console.WriteLine($"{notas20} nota(s) de R$ 20,00");
+            Console.WriteLine($"{notas10} nota(s) de R$ 10,00");
+            Console.WriteLine($"{notas5} nota(s) de R$ 5,00");
+            Console.WriteLine($"{notas2} nota(s) de R$ 2,00");
+            Console.WriteLine($"{notas1} nota(s) de R$ 1,00");
         }
     }
 }
--- Bee1018/Bee1018/obj/Debug/net8.0/Bee1018.AssemblyInfo.cs
+++ Bee1018/Bee1018/obj/Debug/net8.0/Bee1018.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bee1018")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67242c5c9bf7f56b8f273818f1089de5ee686a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bee1018")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bee1018")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
