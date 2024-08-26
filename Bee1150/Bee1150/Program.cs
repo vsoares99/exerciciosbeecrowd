a67242c5c9bf7f56b8f273818f1089de5ee686a6
vsoares99 <victor.o.soares@hotmail.com>
2024-08-26 16:24:28 -0300

    first commit

---
+++ Bee1150/Bee1150/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, z;
+            x = int.Parse(Console.ReadLine());
+
+            do
+            {
+                z = int.Parse(Console.ReadLine());
+            } while (z <= x);
+
+            int somaNumeros = 0;
+            int numeroAtual = x;
+            int contador = 0;
+            while (somaNumeros < z)
+            {
+                somaNumeros = somaNumeros + numeroAtual;
+                numeroAtual++;
+                contador++;
+            }
+
+            Console.WriteLine(contador);
+        }
+    }
+}
