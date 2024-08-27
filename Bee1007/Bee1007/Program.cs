58683169bcc80194c4b602836700869041a8a8d4
vsoares99 <victor.o.soares@hotmail.com>
2024-08-27 15:15:55 -0300

    Bee1007 Resolvido

---
+++ Bee1007/Bee1007/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, diferenca;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+
+            diferenca = (A * B) - (C * D);
+
+            Console.WriteLine($"DIFERENCA = {diferenca}");
+        }
+    }
+}
