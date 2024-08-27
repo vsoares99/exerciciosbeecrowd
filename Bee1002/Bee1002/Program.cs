268e990d2b0669f0b3966151d8906236ecf26534
vsoares99 <victor.o.soares@hotmail.com>
2024-08-27 13:32:37 -0300

    Bee1002 Resolvido

---
+++ Bee1002/Bee1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159;
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = n * (raio * raio);
+
+            Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
