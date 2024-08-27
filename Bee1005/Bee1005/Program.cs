5351b985390c6dc76ac253832c2096ffdcfd5b0d
vsoares99 <victor.o.soares@hotmail.com>
2024-08-27 14:36:05 -0300

    Bee1005 Resolvido

---
+++ Bee1005/Bee1005/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (A * 3.5 + B * 7.5) / (3.5 + 7.5);
+
+            Console.WriteLine($"MEDIA = {media.ToString("F5", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
