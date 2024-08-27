79dee86a6949331068ed7fb803c24067ce640f42
vsoares99 <victor.o.soares@hotmail.com>
2024-08-27 18:52:20 -0300

    Bee1010 Resolvido

---
+++ Bee1010/Bee1010/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] linha1 = Console.ReadLine().Split(' ');
+
+            int codigo = int.Parse(linha1[0]);
+            int qtd = int.Parse(linha1[1]);
+            double preco = double.Parse(linha1[2], CultureInfo.InvariantCulture);
+
+            string[] linha2 = Console.ReadLine().Split(' ');
+
+            int codigo2 = int.Parse(linha2[0]);
+            int qtd2 = int.Parse(linha2[1]);
+            double preco2 = double.Parse(linha2[2], CultureInfo.InvariantCulture);
+
+            double soma = (qtd * preco) + (qtd2 * preco2);
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {soma.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
