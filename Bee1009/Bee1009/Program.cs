eb44cb8537a216fb24061b2791fc7df99180d383
vsoares99 <victor.o.soares@hotmail.com>
2024-08-27 18:40:50 -0300

    Bee1009 Resolvido

---
+++ Bee1009/Bee1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioFixo, vendas, totalSalario;
+
+            nome = Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            totalSalario = vendas * 0.15 + salarioFixo;
+
+            Console.WriteLine($"TOTAL  = R$ {totalSalario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
