de45847ac8c7258b0f94ee3c67ee1416838411d4
vsoares99 <victor.o.soares@hotmail.com>
2024-08-27 19:13:20 -0300

    Bee1011 Resolvido

---
+++ Bee1011/Bee1011/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double volume = (4.0 / 3) * pi * raio * raio * raio;
+
+            Console.WriteLine($"VOLUME  = {volume.ToString("F3", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
