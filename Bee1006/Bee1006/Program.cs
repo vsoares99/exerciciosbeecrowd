219644958069264c356b7001d76b1782508c0fc2
vsoares99 <victor.o.soares@hotmail.com>
2024-08-27 14:58:21 -0300

    Bee1006

---
+++ Bee1006/Bee1006/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, media;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (A * 2.0 + B * 3.0 + C * 5.0) / (2.0 + 3.0 + 5.0);
+
+            Console.WriteLine($"MEDIA = {media.ToString("F1", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
