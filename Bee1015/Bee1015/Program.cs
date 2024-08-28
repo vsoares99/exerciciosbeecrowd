f1fec4ab9346e5825d78548a9453b85879901360
vsoares99 <victor.o.soares@hotmail.com>
2024-08-28 19:43:56 -0300

    Bee1015 Resolvido

---
+++ Bee1015/Bee1015/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, y1, x2, y2, distancia;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            x1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            y1 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+
+            string[] valores2 = Console.ReadLine().Split(' ');
+            x2 = double.Parse(valores2[0], CultureInfo.InvariantCulture);
+            y2 = double.Parse(valores2[1], CultureInfo.InvariantCulture);
+
+            distancia = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            Console.WriteLine(distancia.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
