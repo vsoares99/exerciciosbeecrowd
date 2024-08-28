a53dfb07e6225f21fb4b4d045610ea0dafb8b5f8
vsoares99 <victor.o.soares@hotmail.com>
2024-08-28 19:22:38 -0300

    Bee1014 Resolvido

---
+++ Bee1014/Bee1014/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y, consumo;
+            
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumo = X / Y;
+
+            Console.WriteLine($"{consumo.ToString("F3", CultureInfo.InvariantCulture)} km/l");
+        }
+    }
+}
