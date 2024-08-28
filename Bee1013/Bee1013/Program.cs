dc5e01fabc2c986d37342c87fdc5f9042102fe1f
vsoares99 <victor.o.soares@hotmail.com>
2024-08-28 19:15:57 -0300

    Bee1013 Resolvido

---
+++ Bee1013/Bee1013/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, maiorAB, maior;
+            string[] valores = Console.ReadLine().Split(' ');
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+            C = int.Parse(valores[2]);
+
+            maiorAB = (A + B + Math.Abs(A - B)) / 2;
+
+            maior = (maiorAB + C + Math.Abs(maiorAB - C)) / 2;
+
+            Console.WriteLine($"{maior} eh o maior");
+        }
+    }
+}
