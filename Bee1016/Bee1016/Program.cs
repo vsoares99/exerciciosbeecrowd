4842fd4ab2840c54f3fcd8d3342858a1ae4e8978
vsoares99 <victor.o.soares@hotmail.com>
2024-08-29 15:21:21 -0300

    Bee1016 Resolvido

---
+++ Bee1016/Bee1016/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia = int.Parse(Console.ReadLine());
+
+            int tempo = distancia * 2;
+
+            Console.WriteLine($"{tempo} minutos");
+        }
+    }
+}
