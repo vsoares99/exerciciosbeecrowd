194c9e3530029394acf30c68b16b65ee86ba6e08
vsoares99 <victor.o.soares@hotmail.com>
2024-08-29 19:27:45 -0300

    Bee1020 Resolvido

---
+++ Bee1020/Bee1020/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int ano = N / 365;
+            int mes = (N % 365) / 30;
+            int dia = (N % 365) % 30;
+
+            Console.WriteLine($"{ano} ano(s)");
+            Console.WriteLine($"{mes} mes(es)");
+            Console.WriteLine($"{dia} dia(s)");
+        }
+    }
+}
