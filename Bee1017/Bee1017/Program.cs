fced93cdf8858b1526f3a7da15062be2a5dde45a
vsoares99 <victor.o.soares@hotmail.com>
2024-08-29 15:33:31 -0300

    Bee1017 Resolvido

---
+++ Bee1017/Bee1017/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine());
+
+            double consumo = velocidade * tempo / 12.0;
+
+            Console.WriteLine(consumo.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
