bd7afcafa89970cd1def040872a843dc8d717260
vsoares99 <victor.o.soares@hotmail.com>
2024-08-29 19:05:48 -0300

    Bee1019 Resolvido

---
+++ Bee1019/Bee1019/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N =int.Parse(Console.ReadLine());
+
+            int horas = N / 3600;
+            int minutos = (N % 3600) / 60;
+            int segundos = N % 60;
+
+            Console.WriteLine($"{horas}:{minutos}:{segundos}");
+        }
+    }
+}
